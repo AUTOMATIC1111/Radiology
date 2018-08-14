b3b6bda18a081d94f722aea2ae96bfbfcb421f62
AUTOMATIC1111 <16777216c@gmail.com>
2018-08-14 20:22:07 +0300

    Added mutations: blink, vomiting, set skill

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.415.2203")]
-[assembly: AssemblyFileVersion("1.0.415.2203")]
+[assembly: AssemblyVersion("1.0.427.2019")]
+[assembly: AssemblyFileVersion("1.0.427.2019")]
---
+++ Source/MutationSetSkill.cs
@@ -0,0 +1,52 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Radiology
+{
+    public class MutationSetSkillDef : HediffMutationDef
+    {
+        public MutationSetSkillDef() { hediffClass = typeof(MutationSetSkill); }
+
+        public SkillDef skill;
+        public int setTo;
+    }
+
+    public class MutationSetSkill : Mutation<MutationSetSkillDef>
+    {
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref storedLevel, "storedLevel");
+        }
+
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+
+            SkillRecord rec = pawn.skills.GetSkill(def.skill);
+            if (rec == null) return;
+
+            storedLevel = rec.Level;
+            rec.Level = def.setTo;
+        }
+
+        public override void PostRemoved()
+        {
+            base.PostRemoved();
+
+            SkillRecord rec = pawn.skills.GetSkill(def.skill);
+            if (rec == null) return;
+
+            if (storedLevel != -1) rec.Level = storedLevel;
+        }
+
+        public int storedLevel = -1;
+    }
+
+}
